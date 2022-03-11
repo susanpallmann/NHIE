48157d61ccedbee99382b60ba5ac82161e545348
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2022-03-11 16:33:19 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -25,52 +25,56 @@ function getNewRandomNumber() {
     }
 }
 
+function generatePrompt() {
+    // user has pressed space
+    let promptNumber = getNewRandomNumber();
+    seenPrompts.push(promptNumber);
+
+    $('#prompt').fadeToggle(800);
+    $('#number').fadeToggle(800);
+
+    setTimeout(function () {
+
+        $("#prompt").text(prompts[promptNumber]);
+        $('#prompt').fadeToggle();
+
+        $("#number").text('#' + promptNumber);
+        $('#number').fadeToggle();
+
+        let hue = getRandomNumber(1, 360);
+        $('body').css('background-color', `hsl(${hue}, 70%, 70%)`);
+
+        if (hue > 30 && hue < 231) {
+
+            $('body').removeClass('dark');
+            $('body').addClass('light');
+
+        } else {
+
+            $('body').removeClass('light');
+            $('body').addClass('dark');
+        }
+
+        let newPattern = getRandomNumber(0, 8);
+        $('body').removeClass('pattern-squiggle');
+        $('body').removeClass('pattern-bubble');
+        $('body').removeClass('pattern-wave');
+        $('body').removeClass('pattern-clouds');
+        $('body').removeClass('pattern-blobs');
+        $('body').removeClass('pattern-squares');
+        $('body').removeClass('pattern-triangles');
+        $('body').removeClass('pattern-hexagons');
+        $('body').removeClass('pattern-random');
+        $('body').addClass(patterns[newPattern]);
+
+    }, 800);
+}
+
 $('document').ready(function() {
+    generatePrompt();
     $('body').keyup(function(e) {
         if (e.keyCode == 32) {
-            // user has pressed space
-            let promptNumber = getNewRandomNumber();
-            seenPrompts.push(promptNumber);
-            
-            $('#prompt').fadeToggle(800);
-            $('#number').fadeToggle(800);
-            
-            setTimeout(function () {
-                
-                $("#prompt").text(prompts[promptNumber]);
-                $('#prompt').fadeToggle();
-                
-                $("#number").text('#' + promptNumber);
-                $('#number').fadeToggle();
-                
-                let hue = getRandomNumber(1, 360);
-                $('body').css('background-color', `hsl(${hue}, 70%, 70%)`);
-                
-                if (hue > 30 && hue < 231) {
-                    
-                    $('body').removeClass('dark');
-                    $('body').addClass('light');
-                    
-                } else {
-                    
-                    $('body').removeClass('light');
-                    $('body').addClass('dark');
-                }
-                
-                let newPattern = getRandomNumber(0, 8);
-                $('body').removeClass('pattern-squiggle');
-                $('body').removeClass('pattern-bubble');
-                $('body').removeClass('pattern-wave');
-                $('body').removeClass('pattern-clouds');
-                $('body').removeClass('pattern-blobs');
-                $('body').removeClass('pattern-squares');
-                $('body').removeClass('pattern-triangles');
-                $('body').removeClass('pattern-hexagons');
-                $('body').removeClass('pattern-random');
-                $('body').addClass(patterns[newPattern]);
-                
-            }, 800);
-            
+            generatePrompt();
         }
     });
 });
