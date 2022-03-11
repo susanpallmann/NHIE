666255fd2480cf96f14c22ddce7e7bdd6fe84ab4
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2022-03-11 16:00:40 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -20,16 +20,34 @@ $('document').ready(function() {
             // user has pressed space
             let promptNumber = getNewRandomNumber();
             seenPrompts.push(promptNumber);
+            
             $('#prompt').fadeToggle(800);
+            $('#number').fadeToggle(800);
+            
             setTimeout(function () {
+                
                 $("#prompt").text(prompts[promptNumber]);
                 $('#prompt').fadeToggle();
-            }, 800);
-            $('#number').fadeToggle(800);
-            setTimeout(function () {
+                
                 $("#number").text(promptNumber);
                 $('#number').fadeToggle();
+                
+                let hue = getRandomNumber(1, 360);
+                $('body').css('background-color', `hsl(${hue}, 70%, 70%)`);
+                
+                if (hue > 30 && hue < 231) {
+                    
+                    $('body').removeClass('dark');
+                    $('body').addClass('light');
+                    
+                } else {
+                    
+                    $('body').removeClass('light');
+                    $('body').addClass('dark');
+                }
+                
             }, 800);
+            
         }
     });
 });
