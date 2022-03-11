9c2dfeadc8aaf35f9e31a9e38241239e618774e2
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2022-03-11 14:50:23 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,6 +1,28 @@
+const promptsLength = prompts.length();
+let seenPrompts = [];
+
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function getNewRandomNumber() {
+    let rolledNumber = getRandomNumber(1, promptsLength);
+    if (seenPrompts.includes(rolledNumber)) {
+        console.log("we've seen this one before");
+        getNewRandomNumber();
+    } else {
+        return rolledNumber;
+    }
+}
+
+$('document').ready(function() {
+});
+
 $('body').keyup(function(e) {
     if (e.keyCode == 32) {
         // user has pressed space
-        
+        let promptNumber = getNewRandomNumber();
+        console.log(prompts[promptNumber]);
+        seenPrompts.append(promptNumber);
    }
 });
